6794de2a0059456f85d998b25d6c9d98da88e4a2
M. Fatih Rabbani <fatihrbbni@gmail.com>
2021-10-01 19:51:29 +0700

    memperbaiki bug dan memodifikasi kode
    mengubah status = false menjadi return status = false. Menambahkan spasi. Menambahkan  To.Upper().

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Tebak_Huruf
@@ -7,33 +7,39 @@ namespace Tebak_Huruf
     {
         static void Main(string[] args)
         {
-            string kata = "pemrograman";
+            string kata = "PEMROGRAMAN";
             int kesempatan = 5;
             List<string> hurufTebakan = new List<string>{};
 
             Console.WriteLine("Selamat bermain Tebak Kata");
             Console.WriteLine($"Kata ini terdiri dari {kata.Length} huruf");
             Console.WriteLine($"Kamu punya {kesempatan} kali kesempatan");
+            Console.WriteLine("");
+            Console.WriteLine(cekHuruf(kata, hurufTebakan));
 
             while (kesempatan > 0) {
                 Console.Write("Input huruf tebakanmu: ");
                 string input = Console.ReadLine();
+                input = input.ToUpper();
                 hurufTebakan.Add(input);
+                Console.WriteLine("");
 
                 if (cekKata(kata, hurufTebakan)) {
+                    Console.WriteLine(cekHuruf(kata, hurufTebakan));
                     Console.WriteLine($"Selamat anda berhasil menebak kata \"{kata}\"");
                     break;
                 } else if (kata.Contains(input)) {
                     Console.WriteLine("Tebakan anda benar...");
                     Console.WriteLine(cekHuruf(kata, hurufTebakan));
                 } else {
-                    Console.WriteLine("Tebakan anda salah...");
-                    kesempatan -= 1;
+                    kesempatan--;
+                    Console.WriteLine("Sayang sekali tebakan anda tidak tepat...");
                     Console.WriteLine($"Kesempatan anda tinggal {kesempatan} kali lagi");
+                    Console.WriteLine(cekHuruf(kata, hurufTebakan));
                 }
 
                 if (kesempatan == 0) {
-                    Console.WriteLine($"Game Over, kata yang benar adalah {kata}");
+                    Console.WriteLine($"Game Over, kata yang benar adalah \"{kata}\"");
                     Console.WriteLine("bye, bye,...");
                     break;
                 }
@@ -50,7 +56,7 @@ namespace Tebak_Huruf
                 if (hurufTebakan.Contains(c)) {
                     status = true;
                 } else {
-                    status = false;
+                    return status = false;
                 }
             }
             return status;
@@ -58,12 +64,12 @@ namespace Tebak_Huruf
 
         static string cekHuruf(string kata, List<string> hurufTebakan)
         {
-            string hurufBenar ="";
+            string hurufBenar = "";
             for (int i = 0; i < kata.Length; i++) {
                 string c = Convert.ToString(kata[i]);
 
                 if (hurufTebakan.Contains(c)) {
-                    hurufBenar += c;
+                    hurufBenar += " " + c;
                 } else {
                     hurufBenar += " _ ";
                 }
