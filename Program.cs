2bcbcc52885cb4940967704424b9ef053675cf9e
M. Fatih Rabbani <fatihrbbni@gmail.com>
2021-10-04 13:09:21 +0700

    menghilangkan spasi dan menambah dekorasi

--- Program.cs
+++ Program.cs
@@ -6,43 +6,44 @@ namespace Tebak_Huruf
     class Program
     {
         static void Main(string[] args)
-        {
+                {
             string kata = "PEMROGRAMAN";
             int kesempatan = 5;
-            List<string> hurufTebakan = new List<string>{};
+            List<string> hurufTebakan = new List<string> {};
 
-            Console.WriteLine("Selamat bermain Tebak Kata");
-            Console.WriteLine($"Kata ini terdiri dari {kata.Length} huruf");
-            Console.WriteLine($"Kamu punya {kesempatan} kali kesempatan");
-            Console.WriteLine("");
+            Console.WriteLine("Selamat datang di game tebak kata");
+            Console.WriteLine($"kata ini terdiri dari {kata.Length} huruf");
+            Console.WriteLine($"Anda punya {kesempatan} kesempatan");
             Console.WriteLine(cekHuruf(kata, hurufTebakan));
 
             while (kesempatan > 0) {
-                Console.Write("Input huruf tebakanmu: ");
+                Console.Write("Masukan huruf tebakan anda: ");
                 string input = Console.ReadLine();
                 input = input.ToUpper();
                 hurufTebakan.Add(input);
-                Console.WriteLine("");
+                Console.WriteLine("=============================");
 
                 if (cekKata(kata, hurufTebakan)) {
                     Console.WriteLine(cekHuruf(kata, hurufTebakan));
-                    Console.WriteLine($"Selamat anda berhasil menebak kata \"{kata}\"");
+                    Console.WriteLine("Selamat anda telah menamatkan game ini");
+                    Console.WriteLine($"Kata yang berhasil anda tebak adalah \"{kata}\"");
                     break;
                 } else if (kata.Contains(input)) {
-                    Console.WriteLine("Tebakan anda benar...");
+                    Console.WriteLine("Selamat tebakan anda benar");
                     Console.WriteLine(cekHuruf(kata, hurufTebakan));
                 } else {
                     kesempatan--;
-                    Console.WriteLine("Sayang sekali tebakan anda tidak tepat...");
+                    Console.WriteLine("Sayang sekali tebakan anda salah");
                     Console.WriteLine($"Kesempatan anda tinggal {kesempatan} kali lagi");
                     Console.WriteLine(cekHuruf(kata, hurufTebakan));
                 }
 
                 if (kesempatan == 0) {
-                    Console.WriteLine($"Game Over, kata yang benar adalah \"{kata}\"");
-                    Console.WriteLine("bye, bye,...");
+                    Console.WriteLine("Kesempatan anda sudah habis");
+                    Console.WriteLine($"Kata yang benar adalah \"{kata}\"");
                     break;
                 }
+
             }
         }
 
@@ -65,13 +66,14 @@ namespace Tebak_Huruf
         static string cekHuruf(string kata, List<string> hurufTebakan)
         {
             string hurufBenar = "";
+
             for (int i = 0; i < kata.Length; i++) {
                 string c = Convert.ToString(kata[i]);
 
                 if (hurufTebakan.Contains(c)) {
                     hurufBenar += " " + c;
                 } else {
-                    hurufBenar += " _ ";
+                    hurufBenar += " _";
                 }
             }
             return hurufBenar;
